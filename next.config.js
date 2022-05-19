11d4efab0b375239ea082435020f6c87229db6b3
Lantt <laanfor@gmail.com>
2022-05-19 16:41:03 +0800

    fix: add redirects for youtube

--- next.config.js
+++ next.config.js
@@ -4,4 +4,18 @@ const { i18n } = require('./next-i18next.config')
 module.exports = {
   reactStrictMode: true,
   i18n,
+  async redirects() {
+    return [
+      {
+        source: '/youtube',
+        destination: 'https://ethshanghai.org/',
+        permanent: true,
+      },
+      {
+        source: '/youtube.html',
+        destination: 'https://ethshanghai.org/',
+        permanent: true,
+      },
+    ]
+  },
 }
